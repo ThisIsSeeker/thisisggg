ad73acc454da3d0477923b4494d366a08b0bb185
ThisIsSeeker <136300749+ThisIsSeeker@users.noreply.github.com>
2024-01-09 13:12:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-function loadScript(src) {
-     return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-        
-    });
+
+
+
+
+const addItem = async (item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div);
+
+
 }
-const scriptUrl = 'script.js';
 
-loadScript(scriptUrl)
+const randomDelay = () => {
+    return new Promise((resolve, reject) => {
+        timeout = 1 + 6 * Math.random();
+        setTimeout(() => {
+            resolve()
 
+        }, timeout * 1000);
+    })
 
-   .then(()=>{
+}
+async function main() {
 
-    alert(`Script '${scriptUrl}' loaded successfully`);
+let t = setInterval(() => {
+    let last = document.body.getElementsByTagName("div");
+    last = last[last.length-1];
+    if(last.innerHTML.endsWith("...")){
+        last.innerHTML = last.innerHTML.slice(0,last.innerHTML.length-3)
 
-    }) 
-    .catch((error) => {
-        alert(`This time you are faild to load '${scriptUrl}':`, error);
-    });
+    }
+    else{
+        last.innerHTML = last.innerHTML + ".";
+    }
 
+}, 100);
 
+    let text = ["Initialized Hacking now reading your data",
+        "Reading your files",
+        "Password files detected",
+        "Sending all passwords and personal files to the server",
+        "Cleaning up..."];
+    for (const item of text) {
+        await addItem(item);
 
+    }
+    clearInterval(t);
+}
+main()
